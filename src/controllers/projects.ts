6fd75164e8e9c0441134b2e2136590f1bce7e647
Maxym Berezanskyi <m.berezanskyi@bmlabs.org>
2021-02-07 16:14:37 +0200

    Add inactive filter

--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -4,12 +4,20 @@ import { PrismaClient } from '@prisma/client'
 const router = Router()
 const prisma = new PrismaClient()
 
-router.get('/', async (req, res) => {
+router.get('/', async (_, res) => {
   const projects = await prisma.project.findMany({
+    where: {
+      deleted: false,
+    },
     include: {
       users: {
         include: {
           user: true,
+        },
+        where: {
+          user: {
+            disabled: false,
+          },
         },
       },
       devices: true,
